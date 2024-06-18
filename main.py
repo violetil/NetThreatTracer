275234d4edb969e2217e578bdb58babe883370bb
violet <evelgaden@gmail.com>
2024-06-19 01:27:42 +0800

    Data process and store extended

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP
+import pandas as pd
+
+packet_data = []
+
+def process_packet(packet):
+  if IP in packet:
+    data = {
+      "timestamp": packet.time,
+      "src_ip": packet[IP].src,
+      "dst_ip": packet[IP].dst,
+      "protocol": packet.proto,
+      "length": len(packet)
+    }
+    packet_data.append(data)
 
 
 def packet_callback(packet):
-  print(packet.summary())
+  process_packet(packet)
 
 
 if __name__ == "__main__":
-  sniff(prn=packet_callback, store=0)
+  print("Start capture network traffic data...")
+  sniff(prn=packet_callback, store=0, count=100)
+  df = pd.DataFrame(packet_data)
+  print("Done, write into csv file.")
+  df.to_csv("temp/network_traffic.csv", index=False)

c3da328d0814b53f21ee1b12c3e437771c5438c7
violet <evelgaden@gmail.com>
2024-06-20 22:32:18 +0800

    Add analyze data function(model pred)

--- main.py
+++ main.py
@@ -3,8 +3,30 @@ import torch
 from torch import nn
 from scapy.all import sniff, IP
 
-
+class NetworkTrafficModel(nn.Module):
+  def __init__(self):
+    super(NetworkTrafficModel, self).__init__()
+    self.fc1 = nn.Linear(5, 50)
+    self.fc2 = nn.Linear(50, 3) # Three types of network traffic
+    
+  def forward(self, x):
+    x = torch.relu(self.fc1(x))
+    x = self.fc2(x)
+    return x
+  
+  
+model = NetworkTrafficModel()
+model.load_state_dict(torch.load("models/model.pth"))
+model.eval()
+    
 packet_data = []
+
+
+def analyze_data(data):
+  with torch.inference_mode():
+    input_data = torch.tensor(data).float()
+    output = model(input_data)
+    return output.argmax().item() # Return the result of prediction index
 
   
 def process_packet(packet):

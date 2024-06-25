64bee1e18bd4afe55baaea9556ae4c281765a47f
violet <evelgaden@gmail.com>
2024-06-26 03:08:13 +0800

    Extract flows full features for deep learning model

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 def visualize_data(df):
     plt.figure(figsize=(10, 5))
-    plt.plot(df['timestamp'], df['length'], label="Packet Length")
-    plt.scatter(df['timestamp'], df['prediction'], color="red", label="Predicted Attck Type")
+    plt.plot(df['flow_duration'], df['protocol'], label="Packet Protocol")
+    plt.scatter(df['flow_duration'], df['fwd_packets'], color="red", label="Fwd Packets")
     plt.xlabel("time")
-    plt.ylabel("Packet Length / Attack Type")
+    plt.ylabel("Packet Protocol / Fwd Packets")
     plt.title("Network Traffic and Attack Prediction")
     plt.legend()
     plt.show()

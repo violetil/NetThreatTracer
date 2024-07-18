16fc833d2b72941bdf6553d76e207e51cfdd605f
violet <evelgaden@gmail.com>
2024-07-18 13:23:58 +0800

    Add CNN Model

--- models/network_threat_models.py
+++ models/network_threat_models.py
@@ -82,3 +82,21 @@ class LSTMModel(nn.Module):
     out, _ = self.lstm(x, (h0, c0))
     out = self.fc(out[:, -1, :])
     return out
+
+    
+class CNNModel(nn.Module):
+  def __init__(self, input_dim, num_classes):
+    super(CNNModel, self).__init__()
+    self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
+    self.pool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+    self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+    self.fc1 = nn.Linear(32 * (input_dim // 2 // 2), 64)
+    self.fc2 = nn.Linear(64, num_classes)
+    
+  def forward(self, x):
+    x = self.pool(torch.relu(self.conv1(x)))
+    x = self.pool(torch.relu(self.conv2(x)))
+    x = x.view(x.size(0), -1)
+    x = torch.relu(self.fc1(x))
+    x = self.fc2(x)
+    return x
